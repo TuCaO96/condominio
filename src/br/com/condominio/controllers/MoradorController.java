6bb1470e63ae2c61875cd4e8b696d9bde9451456
Arthur <arthurmr13@hotmail.com>
2016-10-24 23:03:02 -0200

    Arquivos iniciais do projeto

---
+++ src/br/com/condominio/controllers/MoradorController.java
@@ -0,0 +1,20 @@
+package br.com.condominio.controllers;
+
+import br.com.condominio.models.Morador;
+
+/**
+ * @author 71500286 - Arthur Mendonça Ribeiro <71500286@aluno.faculdadecotemig.br>
+ * @author 71401377 - João Vitor Guimarães Gomes <71401377@aluno.faculdadecotemig.br>
+ */
+
+public class MoradorController {
+
+    public boolean criaMorador(){
+
+        Morador morador = new Morador();
+
+
+        return true;
+    }
+
+}
