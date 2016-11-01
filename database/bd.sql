2b50b19bb7c6b30f177c1d44c953d2ed5c08f2e2
Arthur <arthurmr13@hotmail.com>
2016-11-01 19:46:28 -0200

    Banco de dados atualizado

--- database/bd.sql
+++ database/bd.sql
@@ -1,8 +1,39 @@
-create database condominio;
-use condominio;
-
-create table morador(
+CREATE TABLE morador(
   id number(10),
-  nome varchar2(255)
+  usuario_id number(10),
+  apartamento number(10),
+  nome varchar2(255),
+  tipo varchar2(255)
 );
 
+CREATE TABLE usuario(
+  id number(10),
+  login varchar2(255),
+  senha varchar2(255)
+);
+
+CREATE TABLE tipo_despesa(
+  id number(10),
+  descricao varchar2(255)
+);
+
+CREATE TABLE despesa(
+  id number(10),
+  tipo_despesa_id number(10),
+  valor number(10),
+  mes number(2),
+  ano number(4)
+);
+
+CREATE TABLE condominio(
+  id number(10),
+  morador_id number(10),
+  mes number(2),
+  ano number(4),
+  valor number(10),
+  status number(1)
+);
+
+CREATE SEQUENCE seq_morador INCREMENT BY 1;
+
+CREATE SEQUENCE seq_usuario INCREMENT BY 1;
